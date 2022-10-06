72de62adbee5b353aec275830f3a62d058a0c50b
ayang mf <114496518+ayangmff@users.noreply.github.com>
2022-10-06 09:14:38 +0700

    Add files via upload

---
+++ tugaspemogramantiga.py
@@ -0,0 +1,7 @@
+#tugas sesi 3 by ayang marliani f
+nama = "ayang marliani f"
+kelas = "TI22A" 
+jurusan = "teknik informatika"
+quote = "keep it simple but significant"
+
+print(nama,kelas,jurusan,quote)
